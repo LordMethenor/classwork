6b97f2b526109b4deec60102e19cf5666fd517bb
LordMethenor <lordmethenor@outlook.com>
2019-09-16 14:29:16 -0500

    saved to separate files

---
+++ p5/snake.js
@@ -0,0 +1,20 @@
+var lastpontX
+var lastpointY;
+function setup(){
+  createCanvas(600, 400);
+  starting = true
+}
+function draw(){
+  strokeWeight(1)
+  newpointX = mouseX;
+  newpointY = mouseY;
+  if(starting == true){
+    lastpointX = newpointX;
+    lastpointY = newpointY;
+    starting = false;
+  }
+  line(lastpointX, lastpointY, newpointX, newpointY);
+  lastpointX = newpointX;
+  lastpointY = newpointY;
+
+}
