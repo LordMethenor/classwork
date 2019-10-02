4f3ad5794e73dbc6a8b703519eada0d60c43098f
LordMethenor <lordmethenor@outlook.com>
2019-10-02 15:58:07 -0500

    sped up program, moved end game to after drawing finishes

--- p5/sketch.js
+++ p5/sketch.js
@@ -2,7 +2,7 @@
 xList = []
 oldPixels = []
 timePixels = []
-
+over = false
 function setup(){
   createCanvas(400, 400);
   starting = true;
@@ -14,48 +14,51 @@ function setup(){
   }
 }
 function draw(){
-  strokeWeight(5);
-  stroke('rgba(10,10,10,.5)');
-  loadPixels();
-  for (var i = 0; i < pixels.length; i = i + 4) {
-    oldPixels[i] = pixels[i];
-  }
-  var i;
-  xList[xList.length] = mouseX;
-
-  newpointX = mouseX;
-  newpointY = mouseY;
-  if(starting == true){
+  if(over == true){
+    var score = xList.length;
+    var restartText = "The game is over. Your score is " + score + ".";
+    alert(restartText);
+    noLoop()
+  }else{
+    strokeWeight(3);
+    stroke('rgba(10,10,10,.75)');
+    loadPixels();
+    for (var i = 0; i < pixels.length; i = i + 4) {
+      oldPixels[i] = pixels[i];
+    }
+    var i;
+    xList[xList.length] = mouseX;
+  
+    newpointX = mouseX;
+    newpointY = mouseY;
+    if(starting == true){
+      lastpointX = newpointX;
+      lastpointY = newpointY;
+      starting = false;
+    }
+    line(lastpointX, lastpointY, newpointX, newpointY);
     lastpointX = newpointX;
     lastpointY = newpointY;
-    starting = false;
-  }
-  line(lastpointX, lastpointY, newpointX, newpointY);
-  lastpointX = newpointX;
-  lastpointY = newpointY;
-
-  loadPixels();
-  for (var i = 0; i < pixels.length; i = i + 4) {
-    if (pixels[i] !== oldPixels[i]) {
-      if (timePixels[i] == 0) {
-        timePixels[i] = new Date();
-      }else{
-        var endTime = new Date();
-        print(endTime);
-        print(timePixels[i]);
-        var timeDiff = endTime - timePixels[i];
-        timeDiff /= 1000;
-        print(timeDiff);
-        if (timeDiff >= .5) {
-          var score;
-          score = xList.length;
-          var restartText;
-          restartText = "The game is over. Your score is " + score + ".";
-          alert(restartText);
-          noLoop()
-          i = i + pixels.length;
+  
+    loadPixels();
+    for (var i = 0; i < pixels.length; i = i + 4) {
+      if (pixels[i] !== oldPixels[i]) {
+        if (timePixels[i] == 0) {
+          timePixels[i] = new Date();
+        }else{
+          var endTime = new Date();
+          print(endTime);
+          print(timePixels[i]);
+          var timeDiff = endTime - timePixels[i];
+          timeDiff /= 1000;
+          print(timeDiff);
+          if (timeDiff >= .5) {
+            over = true;
+            i = i + pixels.length;
+          }
         }
       }
     }
   }
+    
 }
