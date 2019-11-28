383aad8e58e90defd813a01dea3a5479410c9cdc
LordMethenor <lordmethenor@outlook.com>
2019-11-28 16:26:50 -0600

    made bubble sort algorithm

---
+++ bubble_sort.js
@@ -0,0 +1,16 @@
+
+const bubbleSort = (arr) => {
+  let temp
+  for (let i = arr.length; i > 0; i--)  {
+    for(let j = 0; j < i; j++)  {
+      if(arr[j] > arr[j+1]) {
+        temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr
+}
+arr = [1,78,3]
+console.log(bubbleSort(arr))
