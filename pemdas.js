2f8916aa36b27cfb5e4dbcb981d93612e889bc26
LordMethenor <lordmethenor@outlook.com>
2020-01-28 15:00:39 -0600

    more code

--- pemdas.js
+++ pemdas.js
@@ -1,3 +1,23 @@
-function expression(){
+function expression(input){
+  this.
+}
+function read(input){
+  this.list = input.split("");
+  for (var i = 0; i < this.list.length; i++) {
+    this.ops = ["+","-","*","/","^","(",")","1","2","3","4","5","6","7","8","9"]
+    verified = false
+    for (var j = 0; j < this.ops.length; j++) {
+      if (this.list[i] == this.ops[j]) {
+        verified = true
+        break
+      }
+    }
+
+    if (this.list[i] == '(') {
+      var this.expression = new expression();
+    }
+
+
+  }
 
 }
