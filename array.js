d7c5588f10d0251a370d12f4b2d8b4b85f5cc93e
LordMethenor <lordmethenor@outlook.com>
2020-01-30 09:18:35 -0600

    Added parenthesis, started exponents

--- array.js
+++ array.js
@@ -1,38 +1,40 @@
 function Linked_Array()  {
 
-  this.array = []
-  this.order = []
+  this.array = [];
+  this.order = [];
   this.initArray = function(array)  {
-    this.array = array
+    this.array = array;
     for (var i = 0; i < length(this.array); i++) {
-      this.order[i] = i-1
+      this.order[i] = i-1;
     }
-  }
+  };
   this.insertVal = function(val, loc) {
-    pos = this.getPos(loc)
-    this.array.append(val)
-    this.order[length(this.array)-1] = this.order[pos]
-    this.order[pos] = length(this.array)-1
-  }
+    pos = this.getPos(loc)[0];
+    this.array[length(this.array)] = val;
+    this.order[length(this.array)-1] = this.order[pos];
+    this.order[pos] = length(this.array)-1;
+  };
   this.deleteVal = function(loc)  {
-
-  }
-  this.getPos = function(loc)  {
-    x = -1
-    for (var a = 0, a < loc+1, a++){
-      b = 0
+    pos = this.getPos(loc)[0];
+    this.array.append
+  };
+  this.getPos = function(loc) {
+    x = -1;
+    posList = []
+    for (i = 0, i < loc+1, i++) {
+      b = 0;
       while (this.order[b] != x) {
-        b++
+        b++;
       }
-      x = b
-      this.array[b]
-    }
-    return(x)
+      x = b;
+      posList[length(posList)] = this.array[b];
+    };
+    return([x, posList]);
   }
   this.unlink = function(){
-    this.getPos(length(this.array))
-  }
-  this.getArray
-  this.subArray
-  this.length
-}
+    return(this.getPos(length(this.array))[1]);
+    
+  };
+//  this.getArray;
+//  this.subArray;
+};
--- pemdas.js
+++ pemdas.js
@@ -1,23 +1,82 @@
-function expression(input){
-  this.
-}
 function read(input){
+  this.ans = []
   this.list = input.split("");
   for (var i = 0; i < this.list.length; i++) {
-    this.ops = ["+","-","*","/","^","(",")","1","2","3","4","5","6","7","8","9"]
+    this.ops = ["+","-","*","/","^","(",")","1","2","3","4","5","6","7","8","9",'_']
+    this.nums = ["1","2","3","4","5","6","7","8","9",'_']
     verified = false
     for (var j = 0; j < this.ops.length; j++) {
       if (this.list[i] == this.ops[j]) {
-        verified = true
-        break
+        verified = true;
+        break;
       }
     }
+    if (verified = false){
+      this.list.splice(i,1);
+      i = i-1
+    }else if (this.list[i] == '(') {
+      this.parenthesis = [];
+      this.temp = this.list;
+      this.ans[this.ans.length] = new initialize(this.list.splice(i+1,this.list.length-1));
+      this.list.length = i;
+      this.retval = this.ans[this.ans.length-1].retval;
+      this.list = [].concat(this.list, this.retval);
 
-    if (this.list[i] == '(') {
-      var this.expression = new expression();
+    }else if (this.list[i] == ')')  {
+      this.parenthesis = [];
+      this.ans[this.ans.length] = new initialize(this.list.splice(0,this.list[i]))
+      this.list.splice(0,1)
+      this.retval = this.ans[this.ans.length-1].retval;
+      this.contval = this.retval.length-1
+      this.list = [].concat(this.retval, this.list);
+      i = this.contval
+    }else if (this.list[i] == '^') {
+      this.num1 = []
+      this.num2 = []
+      cont = true
+      r = i-1
+      while (cont == true) {
+        cont = false
+        this.num1[this.num1.length] = this.list[r]
+        r = r-1
+        if  (r == -1) {
+          cont = false;
+        }else {
+          for (var s = 0; s < this.nums.length; s++) {
+            if (this.list[r] == this.nums[s]) {
+              cont = true;
+              break;
+            }
+          }
+        }
+      }
+      this.num1 = this.num1.reverse
+      cont = false
+      r = i+1
+      while (cont == true) {
+        cont = false
+        this.num2[this.num2.length] = this.list[r]
+        r = r+1
+        if  (r == this.list.length) {
+          cont = false;
+        }else {
+          for (var s = 0; s < this.nums.length; s++) {
+            if (this.list[r] == this.nums[s]) {
+              cont = true;
+              break;
+            }
+          }
+        }
+      }
+      this.ans[this.ans.length] = this.num1 ^ this.num2
+      
     }
-
-
   }
 
+}
+function initialize(input){
+  this.input = input;
+
+    }
+  }
 }
