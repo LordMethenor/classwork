7628c064f9edc2690142fd87827cc9184d6c202c
LordMethenor <lordmethenor@outlook.com>
2020-02-10 14:02:45 -0600

    removed redundant count method

--- stack.js
+++ stack.js
@@ -1,26 +1,23 @@
 function Stack() {
-  this.index = [];
+  index = [];
   this.length = 0;
   this.peek = function () {
-    if (this.length == 0) {
-      return "Command failed: stack is empty."
+    if (this.length === 0) {
+      return "Command failed: stack is empty.";
     }
-    return this.index[this.length-1];
+    return index[this.length-1];
   };
   this.pop = function () {
-    if (this.length == 0) {
-      return "Command failed: stack is empty."
+    if (this.length === 0) {
+      return "Command failed: stack is empty.";
     }
     this.length--;
-    return this.index[this.length];
+    return index[this.length];
   };
   this.push = function (val) {
     //if you push nothing, you add 'undefined/placeholder to the stack'
-    this.index[this.length] = val;
+    index[this.length] = val;
     this.length ++;
     return this.length
   };
-  this.count = function () {
-    return this.length;
-  }
 }
