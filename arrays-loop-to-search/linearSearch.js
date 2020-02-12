c80f506eb1fb4b1d2477370dcc19bd57c50ae410
LordMethenor <lordmethenor@outlook.com>
2020-02-12 08:17:03 -0600

    added linear search

---
+++ arrays-loop-to-search/linearSearch.js
@@ -0,0 +1,8 @@
+function linearSearch (array, target) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == target){
+      return i;
+    }
+  }
+  return -1;
+}
--- arrays-loop-to-search/stats.js
+++ arrays-loop-to-search/stats.js
@@ -1,8 +1,28 @@
 function fiveNumSum (data) {
- medianIndex = data.length/2;
- if (medianIndex.isInteger()) {
-   median = (data[medianIndex] + data[medianIndex+1])/2;
- }
- 
-
+  this.data = data;
+  this.data.sort(function(a, b){return a-b});
+  this.min = this.data[0];
+  this.max = this.data[this.data.length-1];
+  this.median = Median(data);
+  this.h1 = this.data;
+  this.h2 = []
+  for (var i = 0; i < this.half; i++){
+    this.h2.push(this.h1.pop());
+    console.log(this.h1)
+    console.log(this.h2)
+  }
+  this.h2.reverse();
+  this.h1.length = this.h2.length
+  console.log(this.h1);
+  console.log(this.h2);
+  this.q1 = Median(this.h1);
+  this.q3 = Median(this.h2);
+  this.IQR = this.q3-this.q1;
+  
+  function Median (sortedData) {
+    this.medianIndex = sortedData.length/2;
+    this.half = Math.floor(medianIndex)
+    return (sortedData[Math.ceil(medianIndex-1)] + sortedData[half])/2;
+  }
+  return([this.min,this.q1,this.median,this.q3,this.max]);
 }
