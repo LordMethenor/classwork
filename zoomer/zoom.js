a48ff0b355d0c5bcbdf5ab75bd20c652bacebd16
LordMethenor <lordmethenor@outlook.com>
2020-09-01 15:29:13 -0500

    added zoom experiment

---
+++ zoomer/zoom.js
@@ -0,0 +1,79 @@
+function Meeting (name, link, password) {
+    this.name = name;
+    this.link = link;
+    this.password = password;
+}
+function Schedule (name) {
+    this.name = name;
+    this.entries = [[0,1],[1440,null]];
+    //add starter
+    this.addMeeting = function (meeting, time) {
+        index = 0;
+        var lastIndex;
+        while (true){
+            entry = this.entries[index];
+            entryTime = entry[0];
+            if (time > entryTime) {
+                lastIndex = index;
+                index = entry[1];
+            }else if (time < entryTime) {
+                this.entries[lastIndex][1] = this.entries.length;
+                this.entries.push([time, index, meeting]);
+                return;
+            }else{
+                //warning...will overwrite old meeting
+                this.entries[index][2] = meeting;
+                return;
+            }
+        }
+    }
+    this.findMeeting = function (time) {
+        index = 0
+        while (true) {
+            entry = this.entries[index];
+            entryTime = entry[0];
+            if (time > entryTime) {
+                index = entry[1];
+            }else if (time < entryTime) {
+                return;
+            }else{
+                return entry[2];
+            }
+        }
+    }
+}
+function test () {
+    school = new Schedule ('A day');
+    meetings = [];
+    test = new Meeting ('Test','https://www.google.com','password');
+    school.addMeeting(test, 15*60+28);
+    first = new Meeting('English','https://zoom.us/j/99755115418','Phillips');
+    second = new Meeting('HOTA', 'https://zoom.us/j/92715886178', '469939');
+    third = new Meeting('CS', 'https://zoom.us/j/4224978034?pwd=SVZpZFczdERoK08rWUFhb3o1SHMyQT09', '');
+    fourth = new Meeting('Biology', 'https://zoom.us/j/95972463627?pwd=VjM1bFJxaTR6enB1dWZKTVRRM0RlUT09','');
+    school.addMeeting(first, 7*60+55);
+    school.addMeeting(second, 9*60+30);
+    school.addMeeting(third, 11*60);
+    school.addMeeting(fourth, 13*60+15);
+    clock = new Date();
+    time = clock.getHours()*60 + clock.getMinutes();
+    currentMeeting = school.findMeeting(time);
+    if (currentMeeting != null) {
+        if (currentMeeting.password != null) {
+            alert(currentMeeting.password);
+        }
+        window.open(currentMeeting.link);
+    }
+    check = setInterval(() => {
+        clock = new Date();
+        time = clock.getHours()*60 + clock.getMinutes();
+        currentMeeting = school.findMeeting(time);
+        if (currentMeeting != null) {
+            if (currentMeeting.password != null) {
+                alert(currentMeeting.password);
+            }
+            window.open(currentMeeting.link);
+        }
+    }, 60000);
+}
+test();
